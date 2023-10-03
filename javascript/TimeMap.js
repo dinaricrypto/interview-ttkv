e40b16cfdcf1ac1c64611df961ba56dd60bfdec1
Brandon Ooi <brandono@gmail.com>
2023-10-03 09:59:34 -0700

    initial

---
+++ javascript/TimeMap.js
@@ -0,0 +1,25 @@
+class TimeMap {
+  constructor() {
+  }
+
+  set(key, value, timestamp) {
+    // Write your code here.
+  }
+
+  get(key, timestamp) {
+    // Write your code here.
+  }
+}
+
+// Example usage
+
+// const timeMap = new TimeMap();
+
+// timeMap.set('foo', 'bar', 1);
+// timeMap.get('foo', 1); //returns 'bar'
+// timeMap.get('foo', 3); //returns 'bar'
+// timeMap.set('foo', 'baz', 3);
+// timeMap.get('foo', 1); // returns 'bar'
+// timeMap.get('foo', 2); // returns 'bar'
+// timeMap.get('foo', 3); // returns 'baz'
+
